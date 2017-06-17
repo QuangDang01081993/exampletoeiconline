27907baf1a088951ed9422afe5fab92bf848ee8e
QuangDang01081993 <quang.dt.na@gmail.com>
2017-06-17 23:42:41 +0700

    init project

---
+++ toeic-web-logic/src/main/java/vn/myclass/web/logic/common/WebConstant.java
@@ -0,0 +1,7 @@
+package vn.myclass.web.logic.common;
+
+/**
+ * Created by QUANG DANG on 6/17/2017.
+ */
+public class WebConstant {
+}
---
+++ toeic-web/src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>Test project</title>
+</head>
+<body>
+<h1>test project</h1>
+</body>
+</html>
